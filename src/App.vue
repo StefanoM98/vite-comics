8c912ace2fa95891077215f1bb901df87f7acacc
StefanoM98 <smartino030@gmail.com>
2023-04-13 15:49:41 +0200

    Add: project scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import ContentSection from './components/ContentSection.vue';
+import AppFooter from './components/AppFooter.vue';
 
+export default {
+  components: {
+    AppHeader,
+    ContentSection,
+    AppFooter
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader />
+  <ContentSection />
+  <AppFooter />
 </template>
 
-<style>
+<style lang="scss">
 
 </style>
